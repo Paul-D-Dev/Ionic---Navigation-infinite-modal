80006257bcc06f6bba7afa5c8e164834b67b1b20
Paul DAO <dao.van.paul@gmail.com>
2021-01-07 11:30:41 +0100

    Share modal base with module

--- src/app/components/shared-components.module.ts
+++ src/app/components/shared-components.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { ModalBaseComponent } from './modal-base/modal-base.component';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    IonicModule
+  ],
+  exports: [ModalBaseComponent], // Share the Modal base component everywhere in the app
 })
 export class SharedComponentsModule { }

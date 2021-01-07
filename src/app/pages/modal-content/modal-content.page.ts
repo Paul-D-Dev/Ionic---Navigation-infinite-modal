ee0884851462ed07cc08386c94a286bf2515c575
Paul DAO <dao.van.paul@gmail.com>
2021-01-07 14:50:22 +0100

    FEAT Model content page : add content array

--- src/app/pages/modal-content/modal-content.page.ts
+++ src/app/pages/modal-content/modal-content.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal-content',
@@ -6,10 +7,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal-content.page.scss'],
 })
 export class ModalContentPage implements OnInit {
+  level = 0;
+  arrayId = 0;
+  nextPage = ModalContentPage;
 
-  constructor() { }
+  array=[
+    {
+      id:1,
+      name: 'Paul',
+      sites: [
+        {
+          id: 1,
+          name: 'fts',
+          deploy: '01/01/2020',
+          client : {
+            id:1,
+            name: 'Ludovic'
+          }
+        },
+        {
+          id: 2,
+          name: 'kraak',
+          deploy: '25/02/2020',
+          client : {
+            id:2,
+            name: 'Camille'
+          }
+    
+        }
+    
+      ]
+    },
+    {
+      id:2,
+      name: 'Mathieu',
+      sites: [
+        {
+          id: 3,
+          name: 'Yummy',
+          deploy: '01/01/2020',
+          client : {
+            id:3,
+            name: 'Mat'
+          }
+        },
+        {
+          id: 4,
+          name: 'D',
+          deploy: '25/02/2020',
+          client : {
+            id:4,
+            name: 'Paul'
+          }
+    
+        }
+    
+      ]
+    }
+  ]
 
-  ngOnInit() {
+  constructor(private modalController: ModalController) {}
+
+  ngOnInit() {}
+
+  close() {
+    this.modalController.dismiss();
   }
 
+  setInfoId(i: number ) {
+    this.arrayId = i;
+  }
 }
